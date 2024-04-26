78206f59be27ff6626d3dcd5842031d3ff0eb0b0
Rodolfo <rodysapateiro@gmail.com>
2024-04-26 11:46:57 -0300

    finalmente

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './App'
-AppRegistry.registerComponent(appName, () => App);
+import Raiz from './Raiz'
+AppRegistry.registerComponent(appName, () => Raiz);
